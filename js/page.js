ffa01c1ea04cce85765bc9eeb1a815ee336f9500
Alex McCormick <littlebigtank@gmail.com>
2013-12-23 10:21:34 +0000

    Version 1.2
    Javascript enabled message and older browser code added

--- js/page.js
+++ js/page.js
@@ -68,3 +68,32 @@ function performInstructions() {
   }
   return output;
 }
+
+/* -- hide div on load ------ */
+$( document ).ready(function() {
+  $("#output").hide();
+});
+
+
+/* -- add functions for older browsers, not my code ------ */
+if (!String.prototype.trim) {
+  String.prototype.trim = function(){ return this.replace(/^\s+|\s+$/g, ''); };
+}
+
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(elt /*, from*/) {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++) {
+      if (from in this && this[from] === elt)
+        return from;
+    }
+
+    return -1;
+  };
+}
